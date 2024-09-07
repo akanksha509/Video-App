37b28985d1437cb73eae619d6755b211712e4b5d
akanksha509 <akankshathakur2311@gmail.com>
2024-09-07 11:19:31 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,12 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
+
+
